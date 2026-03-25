f2b737d07fbbcac5552e022bbbc9fbc7f06c32f8
devfoma <oluebube.madu@yahoo.com>
2026-03-25 04:17:17 +0100

    feat: setup — BentoCard — Reusable bento grid card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import { VaultDashboard } from "@/components/vault";
 
 export default function Home() {
   return (
-    // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <div>
-      {/* <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -61,8 +59,7 @@ export default function Home() {
             Documentation
           </a>
         </div>
-      </main> */}
-      <VaultDashboard />
-    </div>
+      </main>
+    </div> 
   );
 }
